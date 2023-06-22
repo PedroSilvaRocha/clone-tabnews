08907db8c28e3cbb833a018e8932089becfc7e4e
PedroSilvaRocha <73750374+PedroSilvaRocha@users.noreply.github.com>
2023-06-22 23:20:45 +0000

    Adicionado pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function Home() {
+    return <span>
+    <a href="https://www.youtube.com/watch?v=AiIBKcd4m5Q&ab_channel=VolodymyrShynkar"> I Love You Baby (S2)</a>
+    <br></br><br></br>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/AiIBKcd4m5Q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </span>
+}
+
+export default Home;
